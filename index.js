cb3d8942d6fc6345a3f5925900e17b48905653d2
Peter Gyory <ptgyory@gmail.com>
2021-06-23 15:56:37 -0600

    Mock up endpoints

--- index.js
+++ index.js
@@ -5,23 +5,40 @@ let host = undefined;
 const clients = [];
 
 wss.on('connection', function connection(ws) {
+  // WHEN CLIENT SOCKET JOINS GENERATE AND ATTACH ID
   clients.push(ws);
 
   ws.on('message', function incoming(m) {
-    const packet = JSON.parse(m)
+    const packet = JSON.parse(m);
+    ws.send(m);
     
     switch (packet.type) {
-      case 'HOST':
+      case 'CREATE_ROOM':
+        // GENERATE HOST KEY JACKBOX STYLE
         host = ws;
+        // SEND KEY TO HOST on "ROOM_IS_CREATED"
       break;
 
-      case 'HOST_UPDATE':
+      case 'REQUEST_TO_JOIN':
+        // SHOULD HAVE HOST KEY
+        // REQUESTS ID FROM HOST, PASS ALONG WS ID
       break;
 
-      case 'CLIENT_UPDATE':
+      case 'CONFIRM_JOIN':
+        // SHOULD HAVE CLIENT"S PLAYER ID
+        // ALSO SHOULD HAVE CLIENT ID
+      break;
+
+      case 'UPDATE_CLIENT':
+        // FORWARD TO ALL CLIENTS IN ROOM
+      break;
+
+      case 'UPDATE_HOST':
+        // FORWARD TO HOST BASED ON ROOM KEY
       break;
 
       default:
+        // FORWARD TO PLAYER
       break;
     }
   });
--- test/client.js
+++ test/client.js
@@ -10,5 +10,44 @@ socket.addEventListener('open', function (event) {
 
 // Listen for messages
 socket.addEventListener('message', function (event) {
-    console.log('Message from server ', event.data);
+    const packet = JSON.parse(event.data);
+
+    switch (packet.type) {
+      // server -> host
+      case 'ROOM_IS_CREATED':
+        // HOST
+        // RECIEVE KEY FROM HOST
+        // MOVE ON TO HOST SCREEN
+      break;
+
+      // client -> host
+      case 'REQUEST_TO_JOIN':
+        // HOST
+        // assign player ID and forward on "CONFIRM_JOIN"
+        // update player array
+      break;
+
+      // client -> host
+      case 'UPDATE_HOST':
+        // HOST
+        // UPDATE STATE FOR SPECIFIED PLAYER ID
+      break;
+
+      // host -> client
+      case 'CONFIRM_JOIN':
+        // CLIENT
+        // RECIEVE PLAYER ID
+        // MOVE TO HOSTED SCREEN
+        // START RECIEVING HOST UPDATES
+      break;
+
+      // host -> client
+      case 'UPDATE_CLIENT':
+        // CLIENT
+        // PARSE UPDATE FROM HOST
+      break;
+
+      default:
+      break;
+    }
 });
